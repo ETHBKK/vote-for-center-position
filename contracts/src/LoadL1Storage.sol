4b29d17388d2d0423cd101f6398ddf4f8cf0878e
fengshanshan <fengshanshn@icloud.com>
2024-11-16 17:32:49 +0700

    voting contract

---
+++ contracts/src/LoadL1Storage.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+contract LoadL1Storage {
+    address immutable L1SLOAD_PRECOMPILE = 0x0000000000000000000000000000000000000101;
+    address immutable l1sloadContractAddress;
+    // each user just allow to mint one NFT
+    uint256 constant USER_TOKEN_INDEX = 0;
+
+    constructor(address _l1sloadContractAddress) {
+        l1sloadContractAddress = _l1sloadContractAddress;
+    }
+
+    // Retrieve mapping(address => uint256)
+    function retrieveL1AddressToMapping(uint256 variableStorageSlot, address _mappingAddress)
+        public
+        view
+        returns (uint256)
+    {
+        (bool success, bytes memory data) = L1SLOAD_PRECOMPILE.staticcall(
+            abi.encodePacked(
+                l1sloadContractAddress,
+                uint256(keccak256(abi.encodePacked(uint256(uint160(_mappingAddress)), variableStorageSlot)))
+            )
+        );
+        if (!success) {
+            revert("Error extracting mapping data");
+        }
+        return abi.decode(data, (uint256));
+    }
+
+    // Retrieve mapping(address => mapping(uint256 => uint256))
+    function retrieveL1AddressToNestMapping(uint256 variableStorageSlot, address _mappingAddress)
+        public
+        view
+        returns (uint256)
+    {
+        bytes32 initialSlot = keccak256(abi.encodePacked(uint256(uint160(_mappingAddress)), variableStorageSlot));
+        bytes32 finalSlot = keccak256(abi.encodePacked(USER_TOKEN_INDEX, initialSlot));
+
+        (bool success, bytes memory data) =
+            L1SLOAD_PRECOMPILE.staticcall(abi.encodePacked(l1sloadContractAddress, finalSlot));
+        if (!success) {
+            revert("Error extracting nested mapping data");
+        }
+        return abi.decode(data, (uint256));
+    }
+}
