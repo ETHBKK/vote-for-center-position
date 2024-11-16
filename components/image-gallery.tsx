5bf1ceb9d0eda88a16f7914c0c9db1b050310061
Abby L <yu950713@gmail.com>
2024-11-16 20:16:45 +0700

    feat: add vote button

--- components/image-gallery.tsx
+++ components/image-gallery.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import VoteButton from '@/components/vote-button';
 
 export type ImageData = {
   id: number;
@@ -8,19 +9,32 @@ export type ImageData = {
 };
 
 export default function ImageGallery({ images }: { images: ImageData[] }) {
+  const handleVoteClick = (index: number) => {
+    // TODO: call smart contract to vote
+    console.log('Vote button clicked!', index);
+  };
+
   return (
     <div className="container mx-auto px-4 py-8">
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
         {images.map((image, index) => (
-          <div key={index} className="aspect-w-3 aspect-h-2">
-            <Image
-              src={image.src}
-              alt={image.alt}
-              style={{ objectFit: "cover" }} 
-              className="rounded-lg"
-              height={300}
-              width={300}
-            />
+          <div key={index} className="flex flex-col">
+            <div className="text-md font-semibold text-center my-2">
+              {((image.votes !== images[0].votes) && index === 2) ? "👑" : "🗳️"} {Number(image.votes)}
+            </div>
+            <div className="aspect-w-3 aspect-h-2">
+              <Image
+                src={image.src}
+                alt={image.alt}
+                style={{ objectFit: "cover" }}
+                className="rounded-lg"
+                height={300}
+                width={300}
+              />
+            </div>
+            <div className="flex justify-center my-6">
+              <VoteButton onClick={() => handleVoteClick(image.id)} />
+            </div>
           </div>
         ))}
       </div>
