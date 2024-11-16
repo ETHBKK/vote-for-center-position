8fb9c503897ed26c9692c4639f9e615a256be97e
Abby L <yu950713@gmail.com>
2024-11-16 16:04:34 +0700

    feat: add thirdweb context

---
+++ contexts/thirdweb.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { Dispatch, ReactNode, SetStateAction, createContext, useContext, useState } from "react";
+import { ThirdwebClient } from "thirdweb";
+import {
+  ThirdwebProvider,
+} from "thirdweb/react";
+import {
+  createWallet,
+  Wallet,
+} from "thirdweb/wallets";
+import { client } from "@/lib/thirdweb-client";
+
+const wallets = [
+  createWallet("io.metamask"),
+];
+
+interface ITwebContext {
+  client: ThirdwebClient,
+  wallets: (Wallet<"io.metamask"> | Wallet<"com.coinbase.wallet"> | Wallet<"walletConnect"> | Wallet<"inApp">)[],
+  setLoggedIn: Dispatch<SetStateAction<boolean>>,
+  loggedIn: boolean
+}
+const TwebContext = createContext<ITwebContext>({ client, wallets, setLoggedIn: () => { }, loggedIn: false });
+
+export function TwebProvider({ children }: { children: ReactNode }) {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
+
+  return (
+    <TwebContext.Provider value={{
+      client,
+      wallets,
+      setLoggedIn,
+      loggedIn
+    }}>
+      <ThirdwebProvider>
+        {children}
+      </ThirdwebProvider>
+    </TwebContext.Provider>
+  );
+}
+
+export const useTwebContext = () => useContext(TwebContext);
